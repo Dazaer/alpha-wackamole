e7e0c45f23d4e6ceb6f019d079bf6a9e02b97fc9
José Correia <joseph.apc95@gmail.com>
2019-10-13 14:02:46 +0000

    Added mouse hammer functionalities

--- src/org/academiadecodigo/thunderstructs/Hammer.java
+++ src/org/academiadecodigo/thunderstructs/Hammer.java
@@ -7,7 +7,7 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Hammer implements MouseHandler {
 
-    private Picture picture;
+    private Picture hammer;
     private Mouse mouse;
 
     public Hammer () {
@@ -15,11 +15,24 @@ public class Hammer implements MouseHandler {
 
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
-
+        hammer.draw();
+        System.out.println("I just cliked!");
     }
 
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
-
+        hammer = new Picture(mouseEvent.getX()/2 - 20,mouseEvent.getY()/2 - 50, "hammerPicture.png");
+        System.out.println(mouseEvent.toString());
+        hammer.translate(mouseEvent.getX()/2,mouseEvent.getY()/2);
+        System.out.println(hammer.getX() + " " + hammer.getY());
+        System.out.println("I'm moving");
     }
+
+    /*Hammer hammer = new Hammer();
+    Mouse mouse = new Mouse(hammer);
+        hammer.init();
+
+
+        mouse.addEventListener(MouseEventType.MOUSE_CLICKED);
+        mouse.addEventListener(MouseEventType.MOUSE_MOVED); */
 }
